fda0c21aea0fada632a09ffd4bc2d5f7b6238150
Eval EXEC <execvy@gmail.com>
2024-09-17 14:39:34 +0800

    update

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.github.seancorfield/next.jdbc "1.3.939"]
+                 [org.xerial/sqlite-jdbc "3.46.1.0"]
+                 [metosin/jsonista "0.3.10"]
+                 [compojure "1.7.1"]
+                 [ring "1.12.2"]]
   :main ^:skip-aot pb.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
