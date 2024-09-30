d81c9296018da9638481ff3e8abf43e70066830a
Eval EXEC <execvy@gmail.com>
2024-09-30 10:40:42 +0800

    remove all

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to pb
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/pb/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns pb.core-test
-  (:require [clojure.test :refer :all]
-            [pb.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
