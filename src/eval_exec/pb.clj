6980ec1dee5da19a9808c0a572c6195d973353a7
Eval EXEC <execvy@gmail.com>
2024-10-01 22:39:20 +0800

    clojure backend

---
+++ src/eval_exec/pb.clj
@@ -0,0 +1,16 @@
+(ns eval-exec.pb
+  (:gen-class)
+  (:require [ring.adapter.jetty :as jetty]
+            [clojure.pprint]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [eval-exec.handle :as handle]
+            ))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (handle/db-init)
+  (jetty/run-jetty
+   (wrap-reload
+    handle/app)
+   {:port 8080 :join? true}))
---
+++ test/eval_exec/pb_test.clj
@@ -0,0 +1,7 @@
+(ns eval-exec.pb-test
+  (:require [clojure.test :refer :all]
+            [eval-exec.pb :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
