73b2f8d3ee5d8ee76c64dae3aafc22502503a292
Eval EXEC <execvy@gmail.com>
2024-10-03 16:17:40 +0800

    update

--- src/clj/handle.clj
+++ src/clj/handle.clj
@@ -1,9 +1,14 @@
 (ns clj.handle
-  (:require [jsonista.core :as j]
-            [clojure.tools.logging :as logging]
-            [compojure.route]
-            [compojure.core]
-            [next.jdbc :as jdbc]))
+  (:require
+   [jsonista.core :as j]
+   [clojure.tools.logging :as logging]
+   [ring.middleware.json]
+   [compojure.route]
+   [ring.util.response]
+   [compojure.core]
+   [next.jdbc :as jdbc]
+   [next.jdbc.result-set :as rs]
+   [clojure.pprint :as pp]))
 
 (def db {:dbtype "sqlite" :dbname "pastebin.db"})
 (def ds (jdbc/get-datasource db))
@@ -16,14 +21,16 @@
 (defn all-contents
   [request]
   (let [all-contents
-        (jdbc/execute! ds ["SELECT * FROM pastebin"])]
-    (str all-contents)))
+        ;; {:foo "bar"}
+        (jdbc/execute! ds ["SELECT * FROM pastebin"] {:return-keys false :builder-fn rs/as-unqualified-lower-maps})]
+
+    (ring.util.response/response all-contents)))
 
 (defn content-by-id
   [request]
   (let [uuid (:uuid (:params request))
         content  (jdbc/execute-one! ds ["SELECT content FROM pastebin WHERE uuid = ?" uuid])]
-    (:pastebin/content content)))
+    (ring.util.response/response content)))
 
 (defn submit-content
   [request]
@@ -32,12 +39,11 @@
         body-value (j/read-value body j/keyword-keys-object-mapper)
         content (:content body-value)
         uuid (java.util.UUID/randomUUID)
-        url (format "http://127.0.0.1:3000/%s" uuid)]
+        url (format "http://127.0.0.1:8080/%s" uuid)]
     ;; Do something with the body, e.g., save it to a database or process it
     ;; For demonstration purposes, we'll just print the body
     (jdbc/execute! ds ["INSERT INTO pastebin (uuid, content) VALUES (?, ?)" uuid content])
-    {:status 200
-     :body url}))
+    (ring.util.response/response {:url url})))
 
 (defn page-not-found
   [request]
@@ -47,8 +53,8 @@
   (compojure.core/GET "/" []
     (ring.util.response/resource-response
      "index.html" {:root "public"}))
-  (compojure.core/GET "/content/:uuid" params content-by-id) ;; get pastebin content by uuid
-  (compojure.core/GET "/all" [] all-contents) ;; get pastebin homepage
+  (compojure.core/GET "/all" []  all-contents) ;; get pastebin homepage
+  (compojure.core/GET "/:uuid" params content-by-id) ;; get pastebin content by uuid
   (compojure.core/POST "/" params submit-content) ;; submit pastebin content by post body
   (compojure.route/resources "/")
   (compojure.route/not-found  page-not-found))
--- src/clj/pb.clj
+++ src/clj/pb.clj
@@ -3,6 +3,8 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.logger :as logger]
             [clojure.pprint]
+            [ring.middleware.defaults]
+            [ring.middleware.json]
             [ring.middleware.reload :refer [wrap-reload]]
             [clj.handle :as handle]))
 
@@ -11,5 +13,12 @@
   [& args]
   (handle/db-init)
   (jetty/run-jetty
-   (logger/wrap-with-logger (wrap-reload handle/app))
+   (logger/wrap-with-logger
+    ;; (ring.middleware.defaults/wrap-defaults
+    (wrap-reload
+     (ring.middleware.json/wrap-json-response
+      handle/app))
+      ;; ring.middleware.defaults/site-defaults
+	  ;; )
+    )
    {:port 8080 :join? true}))
